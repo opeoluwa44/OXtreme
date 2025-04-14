8a1c336f5829c6856ff2657d974593083940c5a7
opeoluwa44 <opeoluwabello44@gmail.com>
2025-04-14 17:56:05 +0100

    Functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,214 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { FlatList, Pressable, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import Icons from '@/components/Icons'
+import Snackbar from 'react-native-snackbar';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
+const App = () => {
+  const [isCross, setISCross] = useState<boolean>(false)
+  const [gameWinner, setGameWinner] = useState<string>('')
+  const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9))
+
+  const reloadGame =()=>{
+    setISCross(false)
+    setGameWinner('')
+    setGameState(new Array(9).fill('empty', 0, 9))
+  }
+
+  const checkIsWinner =()=>{
+    if (
+      gameState[0] === gameState[1] &&
+      gameState[0] === gameState[2] &&
+      gameState[0] !== 'empty'
+    ) {
+      setGameWinner(`${gameState[0]} won the game!`)
+    }else if (
+      gameState[3] !== 'empty' &&
+      gameState[3] === gameState[4] &&
+      gameState[4] === gameState[5] 
+    ) {
+      setGameWinner(`${gameState[3]} won the game!`)
+    } else if (
+      gameState[6] !== 'empty' &&
+      gameState[6] === gameState[7] &&    
+      gameState[7] === gameState[8]    
+    ) {
+      setGameWinner(`${gameState[6]} won the game!`)
+    }else if (
+      gameState[0] !== 'empty' &&
+      gameState[0] === gameState[3] &&
+      gameState[3] === gameState[6] 
+    ) {
+      setGameWinner(`${gameState[6]} won the game!`)
+    }else if (
+      gameState[1] !== 'empty' &&
+      gameState[1] === gameState[4] &&
+      gameState[4] === gameState[7] 
+    ) {
+      setGameWinner(`${gameState[1]} won the game!`)
+    }else if (
+      gameState[2] === 'empty' &&
+      gameState[2] === gameState[5] &&
+      gameState[5] === gameState[8] 
+    ) {
+      setGameWinner(`${gameState[2]} won the game!`)
+    }else if (
+      gameState[0] === 'empty' &&
+      gameState[0] === gameState[4] &&
+      gameState[4] === gameState[8] 
+    ) {
+      setGameWinner(`${gameState[0]} won the game!`)
+    }else if (
+      gameState[2] === 'empty' &&
+      gameState[2] === gameState[4] &&
+      gameState[4] === gameState[6] 
+    ) {
+      setGameWinner(`${gameState[2]} won the game!`)
+    }else if (!gameState.includes('empty', 0)) {
+      setGameWinner('Draw game....')
+    }
+  }
+
+  const onChangeItem = (itemNumber: number) => {
+    if (gameWinner) {
+      return Snackbar.show({
+        text: gameWinner,
+        backgroundColor: '#000',
+        textColor: '#fff'
+      })
+    }
+
+    if (gameState[itemNumber] === 'empty') {
+      gameState[itemNumber] = isCross ? 'cross' : 'circle'
+      setISCross(!isCross)
+    } else {
+      return Snackbar.show({
+        text: 'Position is already filled',
+        backgroundColor:'red',
+        textColor:'#fff'
+      })
+    }
+
+    checkIsWinner()
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+   <SafeAreaView>
+      <StatusBar/>
+      {gameWinner ? (
+        <View style={[styles.playerInfo, styles.winnerInfo]}>
+          <Text style={styles.winnerTxt}>{gameWinner}</Text>
+        </View>
+      ) : (
+        <View style={[styles.playerInfo, 
+          isCross ? styles.playerX : styles.playerO
+        ]}>
+          <Text>Player{isCross ? 'X' : 'O'}'s Turn</Text>
+        </View>
+      )}
+      <FlatList
+        numColumns={3}
+        data={gameState}
+        style={styles.grid}
+        renderItem={({item, index})=>(
+          <Pressable
+          key={index}
+          style={styles.card}
+          onPress={()=>onChangeItem(index)}
+          >
+            <Icons name={item}/>
+          </Pressable>
+        )}
+      />
+      <Pressable
+        style={styles.gameBtn}
+        onPress={reloadGame}
+      >
+        <Text>
+          {gameWinner ? 'Start new game' : 'Reload game'}
+        </Text>
+      </Pressable>
+   </SafeAreaView>
+  )
 }
 
+export default  App
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  playerInfo: {
+    height:56,
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:4,
+    paddingVertical:8,
+    marginVertical:12,
+    marginHorizontal:14,
+    shadowOffset:{
+      width:1,
+      height:1
+    },
+    shadowColor:'#333',
+    shadowOpacity:0.2,
+    shadowRadius:1.5
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  gameTurnTxt:{
+    fontSize:20,
+    color:'#fff',
+    fontWeight:'600'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  playerX:{
+    backgroundColor:'#38cc77'
   },
-});
+  playerO:{
+    backgroundColor:'#F7CD2E'
+  },
+  grid:{
+    margin:12
+  },
+  card:{
+    height:100,
+    width:'33.33%',
+    alignItems:'center',
+    justifyContent:'center',
+    borderWidth:1,
+    borderColor:'#333'
+  },
+  winnerInfo:{
+    borderRadius:8,
+    backgroundColor:'#38cc77',
+    shadowOpacity:0.1,
+  },
+  winnerTxt:{
+    fontSize: 20,
+    color: '#fff',
+    fontWeight:'600',
+    textTransform:'capitalize'
+  },
+  gameBtn:{
+    alignItems:'center',
+    padding: 10,
+    borderRadius:8,
+    marginHorizontal:36,
+    backgroundColor:'#8d3daf'
+  },
+  gameBtnTxt:{
+    fontSize:18,
+    color:'#fff',
+    fontWeight:'500'
+  },
+
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24
+  },
+  sectionTitle: {
+    fontSize:24,
+    fontWeight: '600'
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize:18,
+    fontWeight:'400'
+  }
+})
